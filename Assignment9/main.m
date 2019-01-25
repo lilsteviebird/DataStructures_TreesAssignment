6cb397ab8bbb1651a171a3474a5dbcc05e748187
lilsteviebird <stevenli880@gmail.com>
2019-01-25 10:39:57 -0500

    Initial Commit

---
+++ Assignment9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment9
+//
+//  Created by sli19 on 1/25/19.
+//  Copyright © 2019 sli19. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
